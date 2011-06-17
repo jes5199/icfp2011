cb445413211c6d49540c2cbcb2e84cfac79d3432
Jesse Wolfe <jes5199@gmail.com>
2011-06-17 15:13:33 -0700

    buildValue uses the state monad to keep track of temp cells used

--- brain/braindriver.hs
+++ brain/braindriver.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Control.Monad.State
 import Strategy
 import Value
 import Card
@@ -7,5 +8,5 @@ import Move
 
 main :: IO ()
 main = do let value = (ValueApplication (ValueApplication (ValueApplication (ValueCard SCard) (ValueApplication (ValueCard KCard) (ValueCard IncCard))) (ValueApplication (ValueCard KCard) (ValueCard ZeroCard))) (ValueCard IdentityCard))
-              result = buildValue [1..255] 0 value
+              result = fst $ runState (buildValue 0 value) [1..255]
           putStr (printMoves result)
