16bfd7984fee30ecd72ff9cb6b357d4de73975a5
Arlo Belshee <arlo.belshee@live.com>
2011-06-17 13:48:38 -0700

    Simulator now does read/eval/print loop.

--- simulator/Simulator.hs
+++ simulator/Simulator.hs
@@ -9,39 +9,38 @@ import Data.Array
 import MoveStep
 import Control.Monad.Error
 
+import CardBehavior
+
 simulate :: GameState -> Move -> GameState
 simulate gameState move = fst $ runMove moveStep gameState
-    where moveStep = do newValue <- apply move
-                        newValue' <- catchError (reduce newValue) (\e -> return (ValueCard IdentityCard))
+    where moveStep = do (leftArg, rightArg) <- takeMove move
+                        newValue' <- catchError (apply leftArg rightArg) (\e -> return (ValueCard IdentityCard))
                         storeResult newValue' move
 
-apply :: Move -> MoveStep Value
-apply (Move applicationDirection card slotNumber)
+takeMove :: Move -> MoveStep (Value, Value)
+takeMove (Move applicationDirection card slotNumber)
     = do oldValue <- getProponentSlotField slotNumber -- TODO: error handling needed if slot number bad???
          case applicationDirection of
-           LeftApplication -> return (ValueApplication (ValueCard card) oldValue)
-           RightApplication -> return (ValueApplication oldValue (ValueCard card))
-
-reduce :: Value -> MoveStep Value
-reduce n@(ValueNum _) = return n
-reduce c@(ValueCard _) = return c
-reduce c@(ValueApplication (ValueCard IdentityCard) x) = incAppCount >> doI x
+           LeftApplication -> return ((ValueCard card), oldValue)
+           RightApplication -> return (oldValue, (ValueCard card))
 
 storeResult :: Value -> Move -> MoveStep ()
 storeResult v (Move _ _ slot) = transformProponentSlots (updateField v slot)
 
 test_Simulator = [
   simulate initialState trivialMove ~?= initialState,
-  runMove (storeResult (ValueNum 0) (Move undefined undefined 3)) initialState ~?= (resultOfMove, Right ()),
-  (runMove (apply $ Move LeftApplication ZeroCard 1) initialState
-   ~?= (initialState, Right (ValueApplication (ValueCard ZeroCard) (ValueCard IdentityCard)))),
-  (runMove (apply $ Move RightApplication ZeroCard 1) initialState
-   ~?= (initialState, Right (ValueApplication (ValueCard IdentityCard) (ValueCard ZeroCard))))
---  simulate initialState FirstPlayer moveWithResult ~?= resultOfMove
+  simulate initialState moveWithResult ~?= overwriteField 3 (ValueCard ZeroCard),
+
+  runMove (storeResult (ValueNum 0) (Move undefined undefined 3)) initialState ~?= (overwriteField 3 (ValueNum 0), Right ()),
+
+  (runMove (takeMove $ Move LeftApplication ZeroCard 1) initialState
+       ~?= (initialState, Right ((ValueCard ZeroCard), (ValueCard IdentityCard)))),
+  (runMove (takeMove $ Move RightApplication ZeroCard 1) initialState
+       ~?= (initialState, Right ((ValueCard IdentityCard), (ValueCard ZeroCard))))
   ]
   where
+    overwriteField slotNum value = GameState FirstPlayer
+                             (updateField value slotNum (firstPlayerBoard initialState))
+                             (secondPlayerBoard initialState) 
     trivialMove = Move LeftApplication IdentityCard 2
     moveWithResult = Move RightApplication ZeroCard 3
-    resultOfMove = GameState FirstPlayer
-                             (updateField (ValueNum 0) 3 (firstPlayerBoard initialState))
-                             (secondPlayerBoard initialState)
