e053d8f58f52ec5265ed921c0f3e8890faf370cc
Jesse Wolfe <jes5199@gmail.com>
2011-06-16 22:17:42 -0700

    driver to translate from paul-speak to game-speak

---
+++ driver/drive_spec.rb
@@ -0,0 +1,16 @@
+
+describe "running drive.rb" do
+  def put_and_get(move)
+    drive = Kernel.open('| ruby drive.rb', 'r+')
+    drive.puts(move)
+    dir = drive.readline.chomp
+    slot = drive.readline.chomp
+    card = drive.readline.chomp
+    return [dir, slot, card]
+  end
+
+  it "should translate concise to expanded" do
+    put_and_get("0 S").should == ['2','0','S']
+    put_and_get("S 0").should == ['1','0','S']
+  end
+end
