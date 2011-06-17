5cd15094927a43f6a30ad4439863e2b0364e53bf
Brian Johnson (MacBook Pro 2) <bjthinks@gmail.com>
2011-06-17 01:32:45 -0700

    Use MoveStep to refactor CardBehavior.
    When executing moves, the game state will be implitily tracked, errors
    will cause execution to stop, the application count will be tracked,
    and exceeding the application count will cause an error.
    
    Note: must call incAppCount for every function application.
    Note: must determine exactly what qualifies as a function application.
    
    Important question: does the zero card count as a function application?
    (It is the only null-ary card...)

--- simulator/CardBehavior.hs
+++ simulator/CardBehavior.hs
@@ -1,67 +1,70 @@
 module CardBehavior (test_CardBehavior) where
 
+import Control.Monad.Error
 import Test.HUnit
 import GameState
 import Value
 import Card
+import MoveStep
 
-type GameStep = GameState -> (GameState,Maybe Value)
+identity :: Value -> MoveStep Value
+identity f = do incAppCount
+                return f
 
-identity :: Value -> GameStep
-identity f state = (state,Just f)
+zero :: MoveStep Value
+zero = do incAppCount -- DOES ZERO COUNT AS A FUNCTION APPLICATION???
+          return (ValueNum 0)
 
-zero :: GameStep
-zero state = (state, Just $ ValueNum 0)
+succ :: Value -> MoveStep Value
+succ (ValueNum n) = do incAppCount
+                       return $ ValueNum $ case n of
+                         65535 -> 65535
+                         _ -> n+1
+succ _ = throwError "succ applied to non-number"
 
-succ :: Value -> GameStep
-succ (ValueNum n) state = (state, Just $ ValueNum m)
-  where m = case n of
-              65535 -> 65535
-              _ -> n+1
-succ _ state = (state, Nothing)
+dbl :: Value -> MoveStep Value
+dbl (ValueNum n) = do incAppCount
+                      return $ ValueNum $ if n > 32767
+                                          then 65535
+                                          else n * 2
+dbl _  = throwError "dbl applied to non-number"
 
-dbl :: Value -> GameStep
-dbl (ValueNum n) state = (state, Just $ ValueNum m)
-  where m = if n > 32767
-              then 65535
-              else n * 2
-dbl _ state = (state, Nothing)
-
-get :: Value -> GameStep
+get :: Value -> MoveStep Value
 get = undefined
 
-put :: Value -> Value -> GameStep
+put :: Value -> Value -> MoveStep Value
 put = undefined
 
-s :: Value -> Value -> Value -> GameStep
+s :: Value -> Value -> Value -> MoveStep Value
 s = undefined
 
-k :: Value -> Value -> GameStep
+k :: Value -> Value -> MoveStep Value
 k = undefined
 
-inc :: Value -> GameStep
+inc :: Value -> MoveStep Value
 inc = undefined
 
-dec :: Value -> GameStep
+dec :: Value -> MoveStep Value
 dec = undefined
 
-attack :: Value -> Value -> Value -> GameStep
+attack :: Value -> Value -> Value -> MoveStep Value
 attack = undefined
 
-help :: Value -> Value -> Value -> GameStep
+help :: Value -> Value -> Value -> MoveStep Value
 help = undefined
 
-copy :: Value -> GameStep
+copy :: Value -> MoveStep Value
 copy = undefined
 
-revive :: Value -> GameStep
+revive :: Value -> MoveStep Value
 revive = undefined
 
-zombie :: Value -> Value -> GameStep
+zombie :: Value -> Value -> MoveStep Value
 zombie = undefined
 
 test_CardBehavior = [
-  identity (ValueNum 3) initialState ~?= (initialState,Just $ ValueNum 3),
-  identity (ValueCard IdentityCard) initialState ~?=
-  (initialState,Just $ ValueCard IdentityCard)
+  runMove (identity (ValueNum 3)) initialState ~?=
+  (initialState,Right $ ValueNum 3),
+  runMove (identity (ValueCard IdentityCard)) initialState ~?=
+  (initialState,Right $ ValueCard IdentityCard)
   ] :: [Test]
