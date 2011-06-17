e487432f5409359e59ab9f7252cde8696c3ec027
Arlo Belshee <arlo.belshee@live.com>
2011-06-17 08:21:00 -0700

    Doesn't even pass tests, but is an interesting sketch

--- simulator/GameState.hs
+++ simulator/GameState.hs
@@ -1,5 +1,6 @@
-module GameState (GameState(..),initialState) where
+module GameState (GameState(..),initialState,updateVitality,updateField,test_GameState) where
 
+import Test.HUnit
 import Data.Array
 import Card
 import Value
@@ -7,13 +8,29 @@ import Value
 data Slot = Slot { vitality :: Int, field :: Value }
           deriving (Eq, Show)
 
+replaceVitality hp slot = Slot hp (field slot)
+replaceField value slot = Slot (vitality slot) value
+
 type Slots = Array Int Slot
 
-data GameState = GameState Slots Slots
+data GameState = GameState { firstPlayerBoard :: Slots, secondPlayerBoard :: Slots }
                deriving (Eq, Show)
+
+transformSlot transformation idx slots = slots // [(idx, transformation (slots ! idx) )]
+
+updateVitality hp idx slots = transformSlot (replaceVitality hp) idx slots
+updateField value idx slots = transformSlot (replaceField value) idx slots
 
 initialState :: GameState
 initialState = GameState a a
   where
     a = array (0,255::Int) [(n,Slot 10000 (cardToValue IdentityCard)) |
                             n <- [0..255]]
+
+test_GameState = [
+    updateVitality 19 1 testSlots ~?= testSlots
+    ]
+    where
+        testSlots = array (0,3::Int) [(n,Slot 10000 (cardToValue IdentityCard)) |
+                            n <- [0..10]]
+        idValue = cardToValue IdentityCard
--- simulator/Simulator.hs
+++ simulator/Simulator.hs
@@ -4,11 +4,25 @@ import Test.HUnit
 import GameState
 import Move
 import Card
+import Value
+import Data.Array
 
-simulate x who y = x
+simulate gameState who move = gameState
+
+apply :: GameState -> Who -> Move -> Value
+apply = undefined
+
+reduce :: Value -> (GameState,Maybe Value)
+reduce = undefined
+
+storeResult :: (GameState, Maybe Value) -> Move -> GameState
+storeResult = undefined
 
 test_Simulator = [
-  simulate initialState FirstPlayer trivialMove ~?= initialState
+  simulate initialState FirstPlayer trivialMove ~?= initialState --,
+--  simulate initialState FirstPlayer moveWithResult ~?= resultOfMove
   ]
   where
     trivialMove = Move LeftApplication IdentityCard 2
+    moveWithResult = Move RightApplication ZeroCard 3
+    resultOfMove = GameState (updateField (ValueNum 0) 3 (firstPlayerBoard initialState)) (secondPlayerBoard initialState)
