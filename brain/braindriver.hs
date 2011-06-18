df263fd7ace7da9906f4a39e7e2fd9770b2a73bb
Markus Roberts <Markus@reality.com>
2011-06-18 13:23:47 -0700

    Pull out strategy/tactics from brain loop

--- brain/braindriver.hs
+++ brain/braindriver.hs
@@ -9,7 +9,14 @@ import Parser
 import System(getArgs)
 
 main :: IO ()
-main = do [arg] <- getArgs
-          let value = parse arg
-              moves = fst $ runState (buildValue 0 (translateValue value)) [1..255]
-          putStr (printMoves (concat [[move, Move LeftApplication IdentityCard 0] | move <- moves]))
+main = do 
+    [arg] <- getArgs
+    let our_moves = planSteps (chooseGoal arg)
+    let their_moves = take (length our_moves) ((Move LeftApplication IdentityCard 0) : their_moves)
+    putStr (printMoves (concat [[our_move, their_move] | our_move <- our_moves, their_move <- their_moves]))
+
+chooseGoal arg = do
+    let value = parse arg
+    translateValue value
+
+planSteps goal = fst $ runState (buildValue 0 goal) [1..255]
