c5b1a947a087dbf74f79ca135990667ed6e3b43c
Jesse Wolfe <jes5199@gmail.com>
2011-06-17 23:08:09 -0700

    runner no longer commits patricide

--- bin/runner.sh
+++ bin/runner.sh
@@ -3,4 +3,4 @@ if [ -z "$1" ] ; then
   echo "./runner.sh ./ltg.linux32 alt < input > output"
   exit
 fi
-( cat ; sleep 1; kill -9 0 ) | ( "$@" 2>&1 )
+setsid bash -c "( cat ; sleep 1; kill -9 0 ) | ( $* 2>&1 )"
