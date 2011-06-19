b052236633b74643ce2e2fd3681a998807f3fc9b
Brian Johnson (MacBook Pro 2) <bjthinks@gmail.com>
2011-06-19 13:11:43 -0700

    showGameStateNicely now is just show

--- simulator/genTestCases.hs
+++ simulator/genTestCases.hs
@@ -529,7 +529,7 @@ testCaseAtomsToMoves testName = testCaseAtomsToMoves'
           testCaseAtomsToMoves' (TestCaseAssertionFailure gs msg : _)
               = error $ unlines $
                 ["Assertion failure in test " ++ testName
-                ,"Game state is: " ++ showGameStateNicely gs
+                ,"Game state is: " ++ show gs
                 ,"Assertion message: " ++ msg
                 ]
 
